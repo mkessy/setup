40a3f9719c9372407777f65ad8b8ab4c5d6fe5ed
Mepuka Kessy <kessy002@umn.edu>
2013-07-10 21:00:46 -0500

    modified:   setup.sh     added vim and spf13 vim downloads, added dotfiles

--- setup.sh
+++ setup.sh
@@ -20,24 +20,27 @@ npm install -g jshint
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
 
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
-sudo apt-add-repository -y ppa:cassou/emacs
-sudo apt-get update
-sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+# Install vim so that you can edit code and stuff
+sudo apt-get install vim
+
+# Install spf13 vim for a bunch of goodies, customization is done below by
+# replacing default .vimrc files with ones from my github account
+curl http://j.mp/spf13-vim3 -L -o - | sh
+
+# Install screen -- already installed on EC2 instances by default?
 
 # git pull and install dotfiles as well
 cd $HOME
 if [ -d ./dotfiles/ ]; then
     mv dotfiles dotfiles.old
-fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
-git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
+
+git clone #REPLACE WITH MEPUKA'S GIT URL for dotfiles
+
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
 
+ln -sb dotfiles/.vimrc
+ln -sb dotfiles/.vimrc.local 
+ln -sb dotfiles/.vimrc.bundles
+ln -sb dotfiles/.vimrc.bundles.local
